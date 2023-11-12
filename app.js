1f8be2ecd392303e494b2ebf4745fcd7bb15acbd
James Ramos <jamesrcramos@gmail.com>
2023-11-11 19:28:43 -0800

    Initial commit - handlebars set up and index page displayed

---
+++ app.js
@@ -0,0 +1,38 @@
+// App.js
+
+/*
+    SETUP
+*/
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 53799;                 // Set a port number at the top so it's easy to change in the future
+
+// app.js
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+// Database
+var db = require('./database/db-connector')
+
+/*
+    ROUTES
+*/
+// app.js
+
+app.get('/', function(req, res)
+    {
+        let query1 = "SELECT * FROM Components;"; 
+        
+        db.pool.query(query1, function(error, rows, fields){
+            res.render('index', {data: rows});                 
+        })   
+    });                                         
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
