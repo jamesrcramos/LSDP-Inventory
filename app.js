7da9259387a849182d32573830d3f2ead6796861
jamesrcramos <jamesrcramos@gmail.com>
2023-11-29 02:50:07 -0500

    Added 'Add' functionality to Manuals entity

--- app.js
+++ app.js
@@ -150,6 +150,45 @@ app.post('/add-equipment-ajax', function(req, res)
     })
 });
 
+app.post('/add-manual-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Manuals (manualName, manualLink) VALUES ('${data.name}', '${data.link}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on Manuals
+            query2 = `SELECT * FROM Manuals;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
 // deleting data
 app.delete('/delete-equipment-ajax/', function(req,res,next){
     let data = req.body;
---
+++ public/js/add_manual.js
@@ -0,0 +1,83 @@
+// Get the objects we need to modify
+let addManualForm = document.getElementById('add-manual-form-ajax');
+
+// Modify the objects we need
+addManualForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputName = document.getElementById("input-name");
+    let inputLink = document.getElementById("input-link");
+
+    // Get the values from the form fields
+    let nameValue = inputName.value;
+    let linkValue = inputLink.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        name: nameValue,
+        link: linkValue,
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-manual-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputName.value = '';
+            inputLink.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Manuals
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("manuals-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let nameCell = document.createElement("TD");
+    let linkCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRow.manualID;
+    nameCell.innerText = newRow.manualName;
+    linkCell.innerText = newRow.manualLink;
+
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(nameCell);
+    row.appendChild(linkCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
